3577683def3bf8788787e48aebb3875f3fa9c693
KristofferStrube <kristoffer.strube@gmail.com>
2023-12-21 22:40:35 +0100

    looping Spectrogram.

--- samples/KristofferStrube.Blazor.WebAudio.WasmExample/Shared/SpectrogramPlot.razor.cs
+++ samples/KristofferStrube.Blazor.WebAudio.WasmExample/Shared/SpectrogramPlot.razor.cs
@@ -1,14 +1,15 @@
 using KristofferStrube.Blazor.WebIDL;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
+using CommunityToolkit.HighPerformance;
+using System;
 
 namespace KristofferStrube.Blazor.WebAudio.WasmExample.Shared;
 
 public partial class SpectrogramPlot : IDisposable
 {
     private bool running;
-    private readonly List<byte[]> data = [];
-    private readonly List<(double start, double length)> intervals = [];
+    private byte[,]? data;
 
     [Inject]
     public required IJSRuntime JSRuntime { get; set; }
@@ -28,17 +29,12 @@ public partial class SpectrogramPlot : IDisposable
     [Parameter]
     public int UpperFrequency { get; set; } = 100;
 
-
-
-    protected override void OnParametersSet()
-    {
-
-    }
-
     protected override async Task OnAfterRenderAsync(bool _)
     {
         if (running || Analyser is null) return;
         running = true;
+
+        data = new byte[UpperFrequency - LowerFrequency, 100];
 
         int bufferLength = (int)await Analyser.GetFrequencyBinCountAsync();
         Uint8Array frequencyDataArray = await Uint8Array.CreateAsync(JSRuntime, Math.Min(bufferLength, UpperFrequency - LowerFrequency));
@@ -48,19 +44,29 @@ public partial class SpectrogramPlot : IDisposable
         while (running)
         {
             await Analyser.GetByteFrequencyDataAsync(frequencyDataArray);
-            data.Add((await frequencyDataArray.GetByteArrayAsync())[LowerFrequency..UpperFrequency]);
+            byte[] reading = await frequencyDataArray.GetByteArrayAsync();
 
             DateTimeOffset currentTime = DateTimeOffset.UtcNow;
-            var intervalLength = (currentTime - lastTime).TotalMilliseconds;
-            intervals.Add(((lastTime - start).TotalMilliseconds, intervalLength));
-
-            if ((currentTime - start).TotalMilliseconds > TimeInSeconds * 1000)
-            {
-                start = DateTimeOffset.UtcNow;
-                data.Clear();
-                intervals.Clear();
-            }
+            int intervalStart = (int)((lastTime - start).TotalMilliseconds / TimeInSeconds / 10);
+            int intervalEnd = (int)((currentTime - start).TotalMilliseconds / TimeInSeconds / 10);
             lastTime = currentTime;
+
+            for (int i = intervalStart; i < intervalEnd; i++)
+            {
+                if (i > 99)
+                {
+                    start = DateTimeOffset.UtcNow;
+                    lastTime = start;
+                    break;
+                }
+                for (int j = 0; j < reading.Length; j++)
+                {
+                    data[j, i] = reading[j];
+                }
+                //Memory2D<byte> readingAs2dMemory = reading.AsMemory2D(UpperFrequency - LowerFrequency, 1);
+                //Memory2D<byte> viewInData = buffer.Slice(0, i, UpperFrequency - LowerFrequency, 1);
+                //readingAs2dMemory.CopyTo(viewInData);
+            }
 
             StateHasChanged();
             await Task.Delay(10);
