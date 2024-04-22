a84514513a9b43f3f3d0f77add7f2974938736a8
KristofferStrube <kristoffer.strube@gmail.com>
2024-04-22 22:20:14 +0200

    Tried out in-process MessagePort.

---
+++ src/KristofferStrube.Blazor.WebAudio/AudioWorklet/MessagePort.InProcess.cs
@@ -0,0 +1,82 @@
+﻿using KristofferStrube.Blazor.DOM;
+using KristofferStrube.Blazor.DOM.Extensions;
+using KristofferStrube.Blazor.WebIDL;
+using Microsoft.JSInterop;
+
+namespace KristofferStrube.Blazor.WebAudio;
+
+/// <summary>
+/// Each <see cref="MessagePortInProcess"/> object can be entangled with another (a symmetric relationship).
+/// Each <see cref="MessagePortInProcess"/> object also has a task source called the port message queue, initially empty.
+/// A port message queue can be enabled or disabled, and is initially disabled.
+/// Once enabled, a port can never be disabled again (though messages in the queue can get moved to another queue or removed altogether, which has much the same effect).
+/// </summary>
+/// <remarks><see href="https://html.spec.whatwg.org/multipage/web-messaging.html#messageport">See the API definition here</see>.</remarks>
+public class MessagePortInProcess : MessagePort, IJSInProcessCreatable<MessagePortInProcess, MessagePort>, IEventTargetInProcess
+{
+    /// <summary>
+    /// An in-process helper.
+    /// </summary>
+    protected readonly IJSInProcessObjectReference inProcessHelper;
+
+    /// <inheritdoc/>
+    public new IJSInProcessObjectReference JSReference { get; }
+
+    /// <inheritdoc/>
+    public static async Task<MessagePortInProcess> CreateAsync(IJSRuntime jSRuntime, IJSInProcessObjectReference jSReference)
+    {
+        return await CreateAsync(jSRuntime, jSReference, new());
+    }
+
+    /// <inheritdoc/>
+    public static async Task<MessagePortInProcess> CreateAsync(IJSRuntime jSRuntime, IJSInProcessObjectReference jSReference, CreationOptions options)
+    {
+        IJSInProcessObjectReference helper = await jSRuntime.GetInProcessHelperAsync();
+        return new(jSRuntime, helper, jSReference, options);
+    }
+
+    /// <inheritdoc cref="CreateAsync(IJSRuntime, IJSInProcessObjectReference, CreationOptions)"/>
+    protected MessagePortInProcess(IJSRuntime jSRuntime, IJSInProcessObjectReference inProcessHelper, IJSInProcessObjectReference jSReference, CreationOptions options) : base(jSRuntime, jSReference, options)
+    {
+        JSReference = jSReference;
+    }
+
+    public void PostMessage(object message, ITransferable[]? transfer = null)
+    {
+        JSReference.InvokeVoid("postMessage", message, transfer?.Select(e => e.JSReference).ToArray());
+    }
+
+    /// <inheritdoc/>
+    public void AddEventListener<TInProcessEvent, TEvent>(string type, EventListenerInProcess<TInProcessEvent, TEvent>? callback, AddEventListenerOptions? options = null)
+         where TEvent : Event, IJSCreatable<TEvent> where TInProcessEvent : IJSInProcessCreatable<TInProcessEvent, TEvent>
+    {
+        this.AddEventListener(inProcessHelper, type, callback, options);
+    }
+
+    /// <inheritdoc/>
+    public void AddEventListener<TInProcessEvent, TEvent>(EventListenerInProcess<TInProcessEvent, TEvent>? callback, AddEventListenerOptions? options = null)
+         where TEvent : Event, IJSCreatable<TEvent> where TInProcessEvent : IJSInProcessCreatable<TInProcessEvent, TEvent>
+    {
+        this.AddEventListener(inProcessHelper, callback, options);
+    }
+
+    /// <inheritdoc/>
+    public void RemoveEventListener<TInProcessEvent, TEvent>(string type, EventListenerInProcess<TInProcessEvent, TEvent>? callback, EventListenerOptions? options = null)
+         where TEvent : Event, IJSCreatable<TEvent> where TInProcessEvent : IJSInProcessCreatable<TInProcessEvent, TEvent>
+    {
+        this.RemoveEventListener(inProcessHelper, type, callback, options);
+    }
+
+    /// <inheritdoc/>
+    public void RemoveEventListener<TInProcessEvent, TEvent>(EventListenerInProcess<TInProcessEvent, TEvent>? callback, EventListenerOptions? options = null)
+         where TEvent : Event, IJSCreatable<TEvent> where TInProcessEvent : IJSInProcessCreatable<TInProcessEvent, TEvent>
+    {
+        this.RemoveEventListener(inProcessHelper, callback, options);
+    }
+
+    /// <inheritdoc/>
+    public bool DispatchEvent(Event eventInstance)
+    {
+        return IEventTargetInProcessExtensions.DispatchEvent(this, eventInstance);
+    }
+}
