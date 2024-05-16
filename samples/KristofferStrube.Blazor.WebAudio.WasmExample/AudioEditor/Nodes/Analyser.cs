69d785eda8ec0930fc7647d5320a570abbc39cc6
KristofferStrube <kristoffer.strube@gmail.com>
2024-05-16 23:21:41 +0200

    Made Analyzer more stable.

--- samples/KristofferStrube.Blazor.WebAudio.WasmExample/AudioEditor/Nodes/Analyser.cs
+++ samples/KristofferStrube.Blazor.WebAudio.WasmExample/AudioEditor/Nodes/Analyser.cs
@@ -24,6 +24,23 @@ public class Analyser : Node
         set => base.Height = 130;
     }
 
+    public string Type
+    {
+        get => Element.GetAttribute("data-type");
+        set
+        {
+            if (value is null)
+            {
+                _ = Element.RemoveAttribute("data-type");
+            }
+            else
+            {
+                Element.SetAttribute("data-type", value);
+            }
+            Changed?.Invoke(this);
+        }
+    }
+
     public bool Running { get; set; }
 
     public override Type Presenter => typeof(AnalyserEditor);
@@ -40,6 +57,7 @@ public class Analyser : Node
             StrokeWidth = "2",
             Height = 130,
             Width = 250,
+            Type = "TimeDomain",
         };
 
         (node.X, node.Y) = SVG.LocalDetransform(SVG.LastRightClick);
