9d6e25dd50208ca0710d4b8541669ea82da907b9
KristofferStrube <KristofferStrube@users.noreply.github.com>
2025-02-06 19:41:54 +0000

    deploy: ea1acdd530ee4dc07e3efda4bc0712447b0af18a

---
+++ js/white-noise.js
@@ -0,0 +1,13 @@
+class WhiteNoiseProcessor extends AudioWorkletProcessor {
+    process(inputs, outputs, parameters) {
+        const output = outputs[0];
+        output.forEach((channel) => {
+            for (let i = 0; i < channel.length; i++) {
+                channel[i] = Math.random() * 2 - 1;
+            }
+        });
+        return true;
+    }
+}
+
+registerProcessor("white-noise", WhiteNoiseProcessor);
