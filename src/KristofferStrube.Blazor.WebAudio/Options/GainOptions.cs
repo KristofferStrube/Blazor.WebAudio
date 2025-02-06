83dd5ce7bc52f9d36730805b891abeb91e42a1ad
KristofferStrube <kristoffer.strube@gmail.com>
2025-02-06 20:02:27 +0100

    Improved XML Docs for GainOptions.

--- src/KristofferStrube.Blazor.WebAudio/Options/GainOptions.cs
+++ src/KristofferStrube.Blazor.WebAudio/Options/GainOptions.cs
@@ -3,13 +3,13 @@
 namespace KristofferStrube.Blazor.WebAudio;
 
 /// <summary>
-/// This specifies options to use in constructing a <see cref="GainNode"/>.
+/// This specifies options for constructing a <see cref="GainNode"/>.
 /// </summary>
 /// <remarks><see href="https://www.w3.org/TR/webaudio/#GainOptions">See the API definition here</see>.</remarks>
 public class GainOptions : AudioNodeOptions
 {
     /// <summary>
-    /// The initial gain value for <see cref="AudioParam.GetValueAsync"/>
+    /// The initial value for <see cref="GainNode.GetGainAsync"/>
     /// </summary>
     [JsonPropertyName("gain")]
     public float Gain { get; set; } = 1;
