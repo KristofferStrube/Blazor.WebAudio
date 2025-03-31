6a0e6ad35fdec6dfa78a14b33fd8285bab67d74f
Kristoffer Strube <kristoffer.strube@gmail.com>
2025-03-31 15:29:15 +0200

    Added tests for `DelayNode`.

--- src/KristofferStrube.Blazor.WebAudio/Options/DelayOptions.cs
+++ src/KristofferStrube.Blazor.WebAudio/Options/DelayOptions.cs
@@ -11,12 +11,18 @@ public class DelayOptions : AudioNodeOptions
     /// <summary>
     /// The maximum delay time for the node. Time is in seconds and must be greater than <c>0</c> and less than <c>3</c> minutes (<c>180</c> seconds).
     /// </summary>
+    /// <remarks>
+    /// The default value is <c>1</c>.
+    /// </remarks>
     [JsonPropertyName("maxDelayTime")]
     public double MaxDelayTime { get; set; } = 1;
 
     /// <summary>
     /// The initial delay time for the node.
     /// </summary>
+    /// <remarks>
+    /// The default value is <c>0</c>.
+    /// </remarks>
     [JsonPropertyName("delayTime")]
     public double DelayTime { get; set; } = 0;
 }
--- tests/IntegrationTests/AudioNodeTests/DelayNodeTest.cs
+++ tests/IntegrationTests/AudioNodeTests/DelayNodeTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.JSInterop;
+﻿using FluentAssertions;
+using Microsoft.JSInterop;
 
 namespace IntegrationTests.AudioNodeTests;
 
@@ -6,4 +7,28 @@ public class DelayNodeTest : AudioNodeWithAudioNodeOptions<DelayNode, DelayOptio
 {
     public override async Task<DelayNode> CreateAsync(IJSRuntime jSRuntime, AudioContext context, DelayOptions? options)
         => await DelayNode.CreateAsync(JSRuntime, AudioContext, options);
+
+    [Test]
+    [TestCase(0)]
+    [TestCase(10)]
+    public async Task GetDelayTimeAsync_ShouldRetrieveDelayTimeParameter(float delayTime)
+    {
+        // Arrange
+        await using DelayNode node = await DelayNode.CreateAsync(JSRuntime, AudioContext, new()
+        {
+            DelayTime = delayTime,
+            MaxDelayTime = delayTime + 1,
+        });
+
+        // Act
+        await using AudioParam delayTimeParameter = await node.GetDelayTimeAsync();
+
+        // Assert
+        float readDelayTime = await delayTimeParameter.GetValueAsync();
+        _ = readDelayTime.Should().Be(delayTime);
+
+        await delayTimeParameter.SetValueAsync(delayTime + 1);
+        readDelayTime = await delayTimeParameter.GetValueAsync();
+        _ = readDelayTime.Should().Be(delayTime + 1);
+    }
 }
